02b5b908517b3e761740b1a8b4837418a69d5c2f
ZhuRuoLing <jkl-963@live.com>
2025-03-24 03:14:29 +0800

    sync from upstream

--- src/main/java/com/github/argon4w/acceleratedrendering/features/items/AcceleratedItemRenderContext.java
+++ src/main/java/com/github/argon4w/acceleratedrendering/features/items/AcceleratedItemRenderContext.java
@@ -1,10 +1,12 @@
 package com.github.argon4w.acceleratedrendering.features.items;
 
+import com.github.argon4w.acceleratedrendering.core.mixins.MCItemColorsAccessor;
 import com.github.argon4w.acceleratedrendering.features.items.mixins.ItemColorsAccessor;
 import lombok.Getter;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.color.item.ItemColor;
 import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.item.ItemStack;
 
@@ -16,14 +18,22 @@ public class AcceleratedItemRenderContext {
 	private final BakedModel	bakedModel;
 	private final RandomSource	random;
 
-	public AcceleratedItemRenderContext(
-			ItemStack		itemStack,
-			BakedModel		bakedModel,
-			RandomSource	random
-	) {
-		this.itemStack	= itemStack;
-		this.itemColor	= ((ItemColorsAccessor) Minecraft.getInstance().getItemColors()).getItemColors().getOrDefault(this.itemStack.getItem(), EmptyItemColor.INSTANCE);
-		this.bakedModel	= bakedModel;
-		this.random		= random;
-	}
+    public AcceleratedItemRenderContext(
+        ItemStack		itemStack,
+        BakedModel		bakedModel,
+        RandomSource	random
+    ) {
+        this.itemStack	= itemStack;
+        this.itemColor	= ((ItemColorsAccessor) Minecraft.getInstance().getItemColors()).getItemColors().getOrDefault(this.itemStack.getItem(), EmptyItemColor.INSTANCE);
+        this.bakedModel	= bakedModel;
+        this.random		= random;
+    }
+
+    private static ItemColor getItemColorOrDefault(ItemStack itemStack) {
+        MCItemColorsAccessor accessor = (MCItemColorsAccessor) Minecraft.getInstance();
+        int id = BuiltInRegistries.ITEM.getId(itemStack.getItem());
+        ItemColor color = ((ItemColorsAccessor) (accessor.getItemColors())).getItemColors().byId(id);
+        if (color == null) return EmptyItemColor.INSTANCE;
+        return color;
+    }
 }
